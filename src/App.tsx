76a4ecb851c21a7a7088c03fb80de5c8adc3d19b
Andrés Ávila <andaviaco@gmail.com>
2021-09-20 01:27:47 -0500

    create the layout using Chakra components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  Input,
+  Text,
+  SimpleGrid,
+} from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Flex align="center" direction="column" min-height="100vh" marginBottom="8">
+      <Flex
+        align="center"
+        width="100%"
+        justify="center"
+        paddingTop="4"
+        paddingBottom="4"
+        marginBottom="8"
+        boxShadow="md"
+      >
+        <Container width="100%" maxWidth="1200px" padding="0 4">
+          <Flex as="nav">Giphy App</Flex>
+        </Container>
+      </Flex>
+
+      <Flex width="100%" height="100%">
+        <Container
+          width="100%"
+          maxWidth="1200px"
+          padding="0 4"
+          alignItems="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Flex direction="column" align="center" marginBottom="8">
+            <Heading as="h1" marginBottom="4" textAlign="center">
+              GIFs.{' '}
+              <Text as="span" fontSize="lg" display="block">
+                GIFs for everyone.
+              </Text>
+            </Heading>
+
+            <form>
+              <HStack spacing="1">
+                <Input placeholder="Cute panda" />
+                <Button colorScheme="purple" variant="outline" type="submit">
+                  Search
+                </Button>
+              </HStack>
+            </form>
+          </Flex>
+
+          <SimpleGrid minChildWidth="120px" spacing="40px">
+            <Box bg="purple.600" height="80px"></Box>
+            <Box bg="purple.600" height="80px"></Box>
+            <Box bg="purple.600" height="80px"></Box>
+            <Box bg="purple.600" height="80px"></Box>
+            <Box bg="purple.600" height="80px"></Box>
+            <Box bg="purple.600" height="80px"></Box>
+          </SimpleGrid>
+        </Container>
+      </Flex>
+    </Flex>
   );
 }
 
